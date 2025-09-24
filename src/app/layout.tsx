2054e777d4e3573c2c37d3931460e3d2c2e029d5
Alexander <stwork87@gmail.com>
2025-09-24 13:06:49 +0300

    added vercel Analitics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import { Analytics } from '@vercel/analytics/react';
 import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import { Suspense } from 'react';
@@ -55,6 +56,7 @@ export default function RootLayout({
             </AnalyticsProvider>
           </Suspense>
         </ThemeProvider>
+        <Analytics />
       </body>
     </html>
   );
